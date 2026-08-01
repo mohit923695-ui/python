d100f4e52f1353afd267f9f3ba3ea0cb1163502d
mohit923695-ui <mohit923695@gmail.com>
2026-08-01 23:31:26 +0530

    changes

--- chapter 4 & ps/questions/problem1.py
+++ chapter 4 & ps/questions/problem1.py
@@ -1,7 +1,21 @@
 #write a program list of fruits 
 
-fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+fruits = []
 
-print(type(fruits))
+f1 = input("enter the fruits name : ")
+fruits.append(f1)
+f2 = input("enter the fruits name : ")
+fruits.append(f2)
+f3 = input("enter the fruits name : ")
+fruits.append(f3)
+f4 = input("enter the fruits name : ")
+fruits.append(f4)
+f5 = input("enter the fruits name : ")
+fruits.append(f5)
+f6 = input("enter the fruits name : ")
+fruits.append(f6)
+f7 = input("enter the fruits name : ")
+fruits.append(f7)
 
-print(fruits[3])
+print(fruits)
+
