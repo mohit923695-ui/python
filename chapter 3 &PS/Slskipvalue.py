9eaf82c77574e42c21eebafb993cc7b87ab2fdbf
mohit923695-ui <mohit923695@gmail.com>
2026-08-01 13:26:02 +0530

    changes

---
+++ chapter 3 &PS/Slskipvalue.py
@@ -0,0 +1,13 @@
+#slicing with skip value 
+word = "amazing"
+print(word[1:6:2])
+
+#second example
+
+a = "0123456789"
+print(a[1:9:4])
+
+#third example
+
+g = "abcdefghijklmnopqrstuvwxyz"
+print(g[1:9:5])
