aca4716f198335ae01821b3bc2f042d5523050f4
mohit923695-ui <mohit923695@gmail.com>
2026-08-01 23:50:07 +0530

    changes

---
+++ chapter 4 & ps/questions/problem3.py
@@ -0,0 +1,5 @@
+#tuples is immutable data type in python. It means once we create a tuple, we cannot modify it. Tuples are defined by enclosing the elements in parentheses ().
+
+t = (1, 2, 3, 4, 5)
+
+t[2] = 10 # This will raise an error because tuples are immutable
