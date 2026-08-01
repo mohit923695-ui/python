0b4dc1ec5bca288ebe57b3793525cf30ae74150a
mohit923695-ui <mohit923695@gmail.com>
2026-08-01 22:57:47 +0530

    changes

---
+++ chapter 4 & ps/lists_methods.py
@@ -0,0 +1,18 @@
+#lists method
+
+
+friends = ["Alice", "Bob", "Charlie", "David", 5 , False]
+
+print(friends[0])
+
+friends.append("mohit")
+print(friends)
+
+friends.reverse()
+print(friends)
+
+friends.insert(3, "3.14")
+print(friends)
+
+friends.remove("Charlie")
+print(friends)
