0adbffe40d0a14a4519eb52a4ed39b853ea2e922
mohit923695-ui <mohit923695@gmail.com>
2026-08-01 13:33:03 +0530

    change

---
+++ chapter 3 &PS/str_futn.py
@@ -0,0 +1,14 @@
+# string functions
+
+a = "mohit"
+len_a  = len(a)
+print(len_a)
+
+print(a.upper())
+print(a.lower())
+
+print(a.count("o"))
+
+print(a.replace("mohit", "nakul"))
+
+print(a.find("h"))  
