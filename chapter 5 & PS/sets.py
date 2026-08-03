37f7c3ca655ff9e93c195bc85f958549fd5f5d29
mohit923695-ui <mohit923695@gmail.com>
2026-08-03 11:49:29 +0530

    changes

---
+++ chapter 5 & PS/sets.py
@@ -0,0 +1,9 @@
+#sets
+
+a = set()    #dont use {} to create an empty set because it will create an empty dictionary instead
+
+s = {1,2,3,4,5,6,7,8,9,4,4,4,}
+
+print(a,type(a))
+
+print(s,type(s))
