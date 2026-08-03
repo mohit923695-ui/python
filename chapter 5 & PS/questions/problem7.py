ec9a1bd8967ab10c42efec23e1fd016e2d8d695d
mohit923695-ui <mohit923695@gmail.com>
2026-08-03 22:12:49 +0530

    changes

---
+++ chapter 5 & PS/questions/problem7.py
@@ -0,0 +1,22 @@
+#if the two friends have the same name, update the value of the key with the new value.
+d = {}
+
+name = input("Enter your name: ")
+leng = input("Enter your lenguages: ")
+d.update({name: leng})
+
+name = input("Enter your name: ")
+leng = input("Enter your lenguages: ")
+d.update({name: leng})
+
+name = input("Enter your name: ")
+leng = input("Enter your lenguages: ")
+d.update({name: leng})
+
+name = input("Enter your name: ")
+leng = input("Enter your lenguages: ")
+d.update({name: leng})
+
+print(d)
+
+#same leng than update the value of the key with the new value.
---
+++ chapter 5 & PS/questions/problem8.py
@@ -0,0 +1,20 @@
+#if the two friends have the same leng, update the value of the key with the new value.
+d = {}
+
+name = input("Enter your name: ")
+leng = input("Enter your lenguages: ")
+d.update({name: leng})
+
+name = input("Enter your name: ")
+leng = input("Enter your lenguages: ")
+d.update({name: leng})
+
+name = input("Enter your name: ")
+leng = input("Enter your lenguages: ")
+d.update({name: leng})
+
+name = input("Enter your name: ")
+leng = input("Enter your lenguages: ")
+d.update({name: leng})
+
+print(d)
