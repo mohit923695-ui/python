b27c8cf4840132fd713c2a00d17834f32c9c2894
mohit923695-ui <mohit923695@gmail.com>
2026-08-03 23:23:34 +0530

    program

---
+++ chapter  6 & PS/conditionals.py
@@ -0,0 +1,11 @@
+# if and else
+
+a = int(input("age of vote "))
+
+if(a >= 18):
+    print("you can vote ")
+
+else:
+    print("you cannot vote")
+
+print("thankyou") 
---
+++ chapter 5 & PS/questions/problem9.py
@@ -0,0 +1,6 @@
+
+# can you change the value 
+
+s = {8,7,12,"mohit",[1,2]}
+
+#no changes
