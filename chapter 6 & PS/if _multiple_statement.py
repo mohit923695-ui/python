55783919195dd337456e4a15d8f8fc8037d5615c
mohit923695-ui <mohit923695@gmail.com>
2026-08-05 01:00:47 +0530

    problem

--- chapter 6 & PS/if _multiple_statement.py
+++ chapter 6 & PS/if _multiple_statement.py
@@ -1,6 +1,10 @@
-# Program to check grade using if-elif-else ladder
 
 marks = int(input("Enter your marks: "))
+#if statemnet 1
+if(marks%2 == 0):
+    print("even numbers")
+
+ #if staement 2
 
 if (marks >= 90):
     print("Grade: A+")
