fd39bfc70d7cb1aa0a2827cee189e776409e0364
mohit923695-ui <mohit923695@gmail.com>
2026-08-06 01:24:29 +0530

    problem7

---
+++ chapter 6 & PS/questions/problem7.py
@@ -0,0 +1,6 @@
+post = input("enter your post: ")
+
+if("Mohit"in post):                  #because m is in capital letter 
+    print("talking about mohit")
+else:
+    print("not talking about mohit")
