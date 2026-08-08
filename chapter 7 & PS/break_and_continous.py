eac9c80f0a82f9b8c1174746035d75b3725f8af0
mohit923695-ui <mohit923695@gmail.com>
2026-08-09 00:16:11 +0530

    break continoues

---
+++ chapter 7 & PS/break_and_continous.py
@@ -0,0 +1,16 @@
+
+#continous stateents 
+
+for i in range(100):
+    if(i==34):
+        continue
+    print(i)
+
+
+
+    #break statements 
+
+for i in range(100):
+    if(i==34):
+        break
+    print(i)
