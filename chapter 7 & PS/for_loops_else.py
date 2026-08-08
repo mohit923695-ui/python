84ecf75f69eebc6646ea953fcb476ce890824468
mohit923695-ui <mohit923695@gmail.com>
2026-08-08 23:47:13 +0530

    changes

---
+++ chapter 7 & PS/for_loops_else.py
@@ -0,0 +1,6 @@
+l = [1,2 3,24 ,35]
+for i in l :
+    print(i)
+
+else:
+    print("done")
---
+++ chapter 7 & PS/for_loops_tuple_list.py
@@ -0,0 +1,19 @@
+#for loops (list)
+
+l = [ 1,23,34,456,5665757]
+
+for i in l :
+    print(i)
+
+    #tuples
+
+    t = (1,34,54,344,3,5)
+    for i in t :
+        print(i)
+
+ #string
+
+
+s = "MOHIT"
+for i in s :    
+    print(i)
