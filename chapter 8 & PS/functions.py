2a725a20c6b22d1b3e49415ecb29da5dc4a9b76a
mohit923695-ui <mohit923695@gmail.com>
2026-08-12 11:53:55 +0530

    function

---
+++ chapter 8 & PS/functions.py
@@ -0,0 +1,25 @@
+#a = int(input("enter the number: "))
+#b = int(input("enter the numbr : "))
+#c = int(input("enter the number :  "))
+
+#average = (a+b+c)/3
+
+#print(average)
+
+#for print any code many times than use function and they also reduce the number of line of code 
+
+def avg():
+    a = int(input("enter the number: "))
+    b = int(input("enter the numbr : "))
+    c = int(input("enter the number :  "))
+
+    average = (a+b+c)/3
+
+    print(average)
+
+avg()
+print("thanks")    
+avg()    
+print("thanks")
+avg()    
+avg()    
