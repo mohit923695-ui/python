fa3919d78d9c97ba29904a2e5369649e72da14c5
mohit923695-ui <mohit923695@gmail.com>
2026-08-13 11:55:49 +0530

    arguments

---
+++ chapter 8 & PS/function_with_arguments.py
@@ -0,0 +1,11 @@
+def goodDay(name , ending):
+    print("good day , " + name )
+    print(ending)
+    return "ok"
+
+a = goodDay("mohit" ,"thank you")
+print(a)
+a = goodDay("rohan" ,"thank you")
+print(a)
+goodDay("nola" ,"thank you")
+goodDay("divya" ,"thanks")
