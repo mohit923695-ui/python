1f98b7852d4910a3576af2787817feb9608b6d6d
mohit923695-ui <mohit923695@gmail.com>
2026-08-17 22:54:24 +0530

    problem_9

---
+++ chapter 8 & PS/questions/problem_9.py
@@ -0,0 +1,9 @@
+#python function to print multiplication table of a given number 
+
+
+def multiply(n):
+    for i in range(1,11):
+        print(f"{n}*{i} = {n*i}")
+
+multiply(5)        
+multiply(23)        
