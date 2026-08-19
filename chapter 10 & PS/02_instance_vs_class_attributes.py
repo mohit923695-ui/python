90554642f4cd5b479797395b43e78035a11618f5
mohit923695-ui <mohit923695@gmail.com>
2026-08-19 17:26:43 +0530

    instance_vs_class_att.

---
+++ chapter 10 & PS/02_instance_vs_class_attributes.py
@@ -0,0 +1,8 @@
+class employee:
+    language = "py"    # this is a class attributes 
+    salary = "1200000"
+
+
+mohit = employee()
+mohit.language = "javascript"     #this is a instance attributes 
+print(mohit.salary,mohit.language)  
