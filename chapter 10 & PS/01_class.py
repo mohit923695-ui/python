aff3f65ed38565c2fdded8ef284a800be64d8a9e
mohit923695-ui <mohit923695@gmail.com>
2026-08-19 17:08:37 +0530

    class

---
+++ chapter 10 & PS/01_class.py
@@ -0,0 +1,16 @@
+class employee:
+    language = "py"    # this is a class attributes 
+    salary = "1200000"
+
+
+mohit = employee()
+mohit.name = "groups"     #this is a object attributes 
+print(mohit.name ,mohit.salary,mohit.language)    
+
+
+rohan = employee()
+rohan.name = "rockstar"
+print(rohan.salary, rohan.language)
+
+
+
