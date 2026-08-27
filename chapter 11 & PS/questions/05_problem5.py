0624788f901607b46412e0b8f6c5c6ee807b0a49
mohit923695-ui <mohit923695@gmail.com>
2026-08-27 19:15:06 +0530

    problem5

---
+++ chapter 11 & PS/questions/05_problem5.py
@@ -0,0 +1,31 @@
+#write a classs 'complex'to represent complex number , using with overloaded operators '+' amd '*' which add and multiple them 
+
+
+
+class vector:
+    def __init__(self,x,y,z ):
+        self.x = x 
+        self.y = y
+        self.z = z 
+
+    def __add__(self, other):
+        print(f"{self.x}+ {other.x},{self.y}+ {other.y},{self.z}+ {other.z}")
+
+    def __mul__(self, other,add):
+        print(f"{self.x} * {other.x},{self.y} * {other.y},{self.z} * {other.z}")           
+
+    def __str__(self,):
+        print(f"vector({self.x}i,{self.y}j,{self.z}k")
+
+
+v1 = vector(1,2,3,)
+v2 =vector(4,5,6) 
+v3 = vector(7,8,9)
+
+print(v1 +v2)
+print(v1 * v2)
+
+
+print(v1 + v3 )
+print(v1 * v3)
+
