6c13f7b1fc5422e0e0b30badd01f296938152a80
mohit923695-ui <mohit923695@gmail.com>
2026-09-02 23:05:07 +0530

    adv

---
+++ chapter 12 & PS/03_adv.py
@@ -0,0 +1,10 @@
+from typing import Union, list , tuple ,dict
+
+number_list : list[int] = [1,2,3]
+
+person : dict[str, str] = {"name":"mohit", "age":"20"}
+
+score : tuple[int, int, int] = (90, 80, 70)
+
+identifier : Union[int, str] = "mohit"
+
