b37e13d65bb0c2ffb58bdfe0434bdf9b2bed1207
mohit923695-ui <mohit923695@gmail.com>
2026-09-02 23:02:13 +0530

    changes

--- chapter 12 & PS/01_walrus.py
+++ chapter 12 & PS/01_walrus.py
@@ -1,5 +1,5 @@
 # use walrus operator to assign and check a value in a single expression
 
 
-if (n := len([1, 2, 3, 4, 5])) > 3:
-    print(f"The length of the list is {n} elements, expected <= 3")
+if (n := len([1,23,4,5,6,7,8,9,10,11])) > 10:    
+    print(f"List is too long ({n} elements, expected <= 10)")
---
+++ chapter 12 & PS/02_types.py
@@ -0,0 +1,11 @@
+
+
+n : int = 10
+
+name : str = "mohit"
+
+
+
+def greet(name:int) -> int:
+    return 0
+
