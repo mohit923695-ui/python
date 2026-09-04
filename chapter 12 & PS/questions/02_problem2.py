8b749b250235d54a365a74484635a1fffe1b3f1d
mohit923695-ui <mohit923695@gmail.com>
2026-09-04 14:09:13 +0530

    problem2

---
+++ chapter 12 & PS/questions/02_problem2.py
@@ -0,0 +1,9 @@
+'''
+write to print third fifth and seventh elements from a list using enumerate functions.
+'''
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i, item in enumerate(l):
+    if i == 2 or i == 4 or i == 6:
+        print(item)
